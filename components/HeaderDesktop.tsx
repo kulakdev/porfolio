1b068bcbc0fc468ff3b3f9d54d5027302e969573
jke <constantinekulak@proton.me>
2023-05-19 10:00:08 +0300

    made the first screen

--- components/HeaderDesktop.tsx
+++ components/HeaderDesktop.tsx
@@ -7,7 +7,7 @@ const HeaderDesktop = () => {
   // const router = useRouter();
   return (
     <div className="hidden lg:block">
-      <header className="flex bg-[green] text-white-soft p-4 mx-auto lg:max-w-[1250px]  fixed md:relative md:flex md:justify-between md:items-center md:text-center">
+      <header className="flex bg-[transparent] mt-[100px] text-white-soft p-4 mx-auto lg:max-w-[1250px]  fixed md:relative md:flex md:justify-between md:items-center md:text-center">
         <h1 className="font-[Epilogue] font-bold text-[64px]">projects</h1>
 
         <HStack>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { url } from "inspector";
 import { NextPage } from "next";
+import Image from "next/image";
 import React, { useLayoutEffect, useRef } from "react";
+import ArrowUpButton from "../components/ArrowUpButton";
 import Wrapper from "../components/Wrapper";
 
 const Index: NextPage = () => {
@@ -18,7 +21,7 @@ const Index: NextPage = () => {
     backgroundPosition: string;
   } = {
     background: "#171717",
-    backgroundImage: "radial-gradient(#656C75 2px, transparent 0)",
+    backgroundImage: "radial-gradient(#656C75 1.1px, transparent 0)",
     backgroundSize: "25px 25px",
     backgroundPosition: "-35px -35px",
   };
@@ -26,18 +29,30 @@ const Index: NextPage = () => {
   return (
     <Wrapper title="">
       <MyComponent />
-      <div className="w-full  flex flex-col" style={dotsStyle}>
-        <div className="mt-[27px] ml-[2px] flex flex-row justify-between">
-          <div className=" bg-grey-soft w-[400px] h-[275px] "></div>
-          <div className=" bg-grey-soft w-[400px] h-[275px]"></div>
-          <div className=" bg-grey-soft w-[400px] h-[275px] mr-[2px]"></div>
+      <div className="w-full h-[calc(100vh-212px)] flex flex-col justify-between">
+        <div style={dotsStyle} className="grow">
+          <div className="mt-[27px] ml-[2px] flex flex-row justify-between">
+            <div className=" bg-grey-soft w-[400px] h-[275px]"></div>
+            <div className=" bg-grey-soft w-[400px] h-[275px]"></div>
+            <div className=" bg-grey-soft w-[400px] h-[275px] mr-[2px]"></div>
+          </div>
+          <div className="mt-[25px] ml-[2px] flex flex-row justify-between">
+            <div className=" bg-grey-soft w-[400px] h-[275px] "></div>
+            <div className=" bg-grey-soft w-[400px] h-[275px]"></div>
+            <div className=" bg-grey-soft w-[400px] h-[275px] mr-[2px]"></div>
+          </div>
         </div>
-        <div className="mt-[25px] ml-[2px] flex flex-row justify-between">
-          <div className=" bg-grey-soft w-[400px] h-[275px] "></div>
-          <div className=" bg-grey-soft w-[400px] h-[275px]"></div>
-          <div className=" bg-grey-soft w-[400px] h-[275px] mr-[2px]"></div>
+        <div className="flex flex-row justify-between mb-[100px] mt-[75px]">
+          <h1 className="justify-self-end font-[Epilogue] font-bold text-[64px] leading-[72px]">
+            about me
+          </h1>
+          <a>
+            <ArrowUpButton />
+          </a>
+          <div className="justify-self-end font-[Epilogue] font-bold text-[64px] leading-[72px] text-transparent select-none">
+            about me
+          </div>
         </div>
-        <h1 className="justify-self-end font-[Epilogue] font-bold text-[64px]">about me</h1>
       </div>
     </Wrapper>
   );
