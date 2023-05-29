215f9051ab6f9f451e0000601a83f7cc366f095f
dead-weight <nibiruassault@gmail.com>
2023-05-29 14:19:03 +0300

    moving to mac

--- components/ButtonNormal.tsx
+++ components/ButtonNormal.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ButtonNormal = ({ text }: { text: string }) => {
   return (
-    <div className="bg-gold text-black-hard font-[Epilogue] size-[20px] align-middle py-3 px-5 w-max h-fit rounded-lg ">
+    <a className="bg-gold text-black-hard font-[Epilogue] size-[20px] align-middle py-3 px-5 w-max h-fit rounded-lg ">
       {text}
-    </div>
+    </a>
   );
 };
 
